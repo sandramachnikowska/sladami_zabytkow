22d585fb0e11cb64538a32aa49720acd3033afb9
Sandra Machnikowska <sandra.machnikowska96@gmail.com>
2023-02-09 22:09:29 +0100

    Extract Widget (WelcomePage) and add FirstPage

--- lib/main.dart
+++ lib/main.dart
@@ -15,35 +15,111 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        backgroundColor: const Color.fromRGBO(224, 212, 212, 1),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'ŚLADAMI ZABYTKÓW',
-                style: GoogleFonts.glory(
-                  fontSize: 48,
-                  fontWeight: FontWeight.bold,
+      home: const WelcomePage(),
+    );
+  }
+}
+
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromRGBO(224, 212, 212, 1),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'ŚLADAMI ZABYTKÓW',
+              style: GoogleFonts.glory(
+                fontSize: 48,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 5,
+              ),
+            ),
+            const SizedBox(height: 20),
+            const Image(
+              image: AssetImage('image/arch.png'),
+              width: 400,
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color.fromRGBO(203, 202, 202, 1),
+                side: const BorderSide(
+                    width: 2, color: Color.fromARGB(255, 42, 41, 41)),
+                elevation: 20,
+              ),
+              child: Text(
+                'Wejdź',
+                style: GoogleFonts.raleway(
+                  fontSize: 30,
+                  color: Colors.black,
                 ),
               ),
-              const SizedBox(height: 20),
-              const Image(
-                image: AssetImage('image/arch.png'),
-                width: 200,
-              ),
-              ElevatedButton(
-                child: Text(
-                  'Wejdź',
-                  style: GoogleFonts.raleway(
-                    fontSize: 20,
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (_) => const FirstPage(),
                   ),
-                ),
-                onPressed: () {},
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class FirstPage extends StatelessWidget {
+  const FirstPage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromRGBO(224, 212, 212, 1),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'DRUGA STRONA',
+              style: GoogleFonts.glory(
+                fontSize: 48,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 5,
               ),
-            ],
-          ),
+            ),
+            const SizedBox(height: 20),
+            const Image(
+              image: AssetImage('image/arch.png'),
+              width: 400,
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color.fromRGBO(203, 202, 202, 1),
+                side: const BorderSide(
+                    width: 2, color: Color.fromARGB(255, 42, 41, 41)),
+                elevation: 20,
+              ),
+              child: Text(
+                'Wejdź',
+                style: GoogleFonts.raleway(
+                  fontSize: 30,
+                  color: Colors.black,
+                ),
+              ),
+              onPressed: () {},
+            ),
+          ],
         ),
       ),
     );
