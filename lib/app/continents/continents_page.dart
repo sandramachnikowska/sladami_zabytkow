788cc3bd559c3a22fcfbe4648a8b13a03f97f20f
Sandra Machnikowska <sandra.machnikowska96@gmail.com>
2023-03-23 19:07:22 +0100

    Add ElevatedButtons with continentals name

--- lib/app/continents/continents_page.dart
+++ lib/app/continents/continents_page.dart
@@ -20,20 +20,163 @@ class ContinentsPage extends StatelessWidget {
           ),
         ),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Świat',
-              style: GoogleFonts.glory(
-                fontSize: 48,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 5,
+      body: ListView(
+        children: [
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: const Color.fromRGBO(203, 202, 202, 1),
+              side: const BorderSide(
+                  width: 2, color: Color.fromARGB(255, 42, 41, 41)),
+              elevation: 20,
+            ),
+            child: Text(
+              'Afryka',
+              style: GoogleFonts.raleway(
+                fontSize: 30,
+                color: Colors.black,
               ),
             ),
-          ],
-        ),
+            onPressed: () {},
+            //  Navigator.of(context).push(
+//MaterialPageRoute(
+            //   builder: (_) =>
+            //  ),
+            // );
+            //},
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: const Color.fromRGBO(203, 202, 202, 1),
+              side: const BorderSide(
+                  width: 2, color: Color.fromARGB(255, 42, 41, 41)),
+              elevation: 20,
+            ),
+            child: Text(
+              'Ameryka Południowa',
+              style: GoogleFonts.raleway(
+                fontSize: 30,
+                color: Colors.black,
+              ),
+            ),
+            onPressed: () {},
+            //  Navigator.of(context).push(
+//MaterialPageRoute(
+            //   builder: (_) =>
+            //  ),
+            // );
+            //},
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: const Color.fromRGBO(203, 202, 202, 1),
+              side: const BorderSide(
+                  width: 2, color: Color.fromARGB(255, 42, 41, 41)),
+              elevation: 20,
+            ),
+            child: Text(
+              'Ameryka Północna',
+              style: GoogleFonts.raleway(
+                fontSize: 30,
+                color: Colors.black,
+              ),
+            ),
+            onPressed: () {},
+            //  Navigator.of(context).push(
+//MaterialPageRoute(
+            //   builder: (_) =>
+            //  ),
+            // );
+            //},
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: const Color.fromRGBO(203, 202, 202, 1),
+              side: const BorderSide(
+                  width: 2, color: Color.fromARGB(255, 42, 41, 41)),
+              elevation: 20,
+            ),
+            child: Text(
+              'Antarktyda',
+              style: GoogleFonts.raleway(
+                fontSize: 30,
+                color: Colors.black,
+              ),
+            ),
+            onPressed: () {},
+            //  Navigator.of(context).push(
+//MaterialPageRoute(
+            //   builder: (_) =>
+            //  ),
+            // );
+            //},
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: const Color.fromRGBO(203, 202, 202, 1),
+              side: const BorderSide(
+                  width: 2, color: Color.fromARGB(255, 42, 41, 41)),
+              elevation: 20,
+            ),
+            child: Text(
+              'Australia',
+              style: GoogleFonts.raleway(
+                fontSize: 30,
+                color: Colors.black,
+              ),
+            ),
+            onPressed: () {},
+            //  Navigator.of(context).push(
+//MaterialPageRoute(
+            //   builder: (_) =>
+            //  ),
+            // );
+            //},
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: const Color.fromRGBO(203, 202, 202, 1),
+              side: const BorderSide(
+                  width: 2, color: Color.fromARGB(255, 42, 41, 41)),
+              elevation: 20,
+            ),
+            child: Text(
+              'Azja',
+              style: GoogleFonts.raleway(
+                fontSize: 30,
+                color: Colors.black,
+              ),
+            ),
+            onPressed: () {},
+            //  Navigator.of(context).push(
+//MaterialPageRoute(
+            //   builder: (_) =>
+            //  ),
+            // );
+            //},
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: const Color.fromRGBO(203, 202, 202, 1),
+              side: const BorderSide(
+                  width: 2, color: Color.fromARGB(255, 42, 41, 41)),
+              elevation: 20,
+            ),
+            child: Text(
+              'Europa',
+              style: GoogleFonts.raleway(
+                fontSize: 30,
+                color: Colors.black,
+              ),
+            ),
+            onPressed: () {},
+            //  Navigator.of(context).push(
+//MaterialPageRoute(
+            //   builder: (_) =>
+            //  ),
+            // );
+            //},
+          ),
+        ],
       ),
     );
   }
